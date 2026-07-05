a36a31cf297bf3ee7f1df7e0155fc9731b7245d2
vibek44 <kshetrivibek@gmail.com>
2026-07-05 21:38:07 +0300

    adding cors for handling external request,and adding test endpoint /api/ping

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
-const PORT = 3000;
-console.log(process);
+const PORT = 3001;
+app.use(cors());
+app.get("/api/ping", (_req, res) => {
+  res.send("pong");
+});
+//console.log(process);
 app.listen(PORT, () => {
   console.log(`app running in port ${PORT}`);
 });
