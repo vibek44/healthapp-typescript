98092b6789e1c1756143615843ca4c916d49765b
vibek44 <kshetrivibek@gmail.com>
2026-07-11 12:52:46 +0300

    importing dianosis and patient handling router and using it to handle /api/diagnoses and /api/patients endpoint

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
+import diagnosesRouter from "./routes/diagnoses.ts";
+import patientsRouter from "./routes/patients.ts";
 const app = express();
-const PORT = 3001;
 app.use(cors());
+app.use(express.json());
+const PORT = 3001;
+
 app.get("/api/ping", (_req, res) => {
-  res.send("pong");
+  res.send("pong..");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
+app.use("/api/patients", patientsRouter);
+
 //console.log(process);
 app.listen(PORT, () => {
   console.log(`app running in port ${PORT}`);
