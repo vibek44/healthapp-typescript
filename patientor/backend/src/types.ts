c97a3ee975bcd9a0eba289be3597f7f6ec4bf755
vibek44 <kshetrivibek@gmail.com>
2026-07-11 17:17:28 +0300

    Adding const GenderObject and type Gender and exporting for use

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -4,11 +4,21 @@ export interface Diagnoses {
   latin?: string;
 }
 
+//this const object can be used both during compile time and run time
+
+export const GenderObject={
+  Male:"male",
+  Female:"female",
+  Other:"other"
+}as const 
+// Gender object type is the union of all the propertie's value 
+export type Gender=typeof GenderObject[keyof typeof GenderObject]
+
 export interface Patient {
   id: string;
   name: string;
   dateOfBirth: string;
-  gender: string;
+  gender: Gender;
   occupation: string;
   ssn: string;
 }
