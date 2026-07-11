284930fc39f365e9b9bfeab23593dff7e500d76c
vibek44 <kshetrivibek@gmail.com>
2026-07-11 12:50:49 +0300

    using Omit utility types to create new type NewPatientEntry from existing interface type Patient and exporting it

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -3,3 +3,15 @@ export interface Diagnoses {
   name: string;
   latin?: string;
 }
+
+export interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  gender: string;
+  occupation: string;
+  ssn: string;
+}
+
+export type PatientEntry = Omit<Patient, "ssn">;
+export type NewPatientEntry = Omit<Patient, "id">;
