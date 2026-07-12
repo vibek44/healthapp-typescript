4f50732bd048364065811a3ac213bb01a202d7f0
vibek44 <kshetrivibek@gmail.com>
2026-07-12 20:04:52 +0300

    creating schema object NewEntrySchema and using it to parse req body data and to develop the required type by zod inferring

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,10 +1,4 @@
-export interface Diagnoses {
-  code: string;
-  name: string;
-  latin?: string;
-}
-
-//this const object can be used both during compile time and run time
+import { z } from "zod"
 
 export const GenderObject={
   Male:"male",
@@ -14,14 +8,27 @@ export const GenderObject={
 // Gender object type is the union of all the propertie's value 
 export type Gender=typeof GenderObject[keyof typeof GenderObject]
 
-export interface Patient {
-  id: string;
+export const NewEntrySchema=z.object({
+  name:z.string().min(3),
+  occupation:z.string().min(2),
+  dateOfBirth:z.iso.date(),
+  gender:z.enum(Object.values(GenderObject)),
+  ssn:z.string().min(9)
+})
+
+export interface Diagnoses {
+  code: string;
   name: string;
-  dateOfBirth: string;
-  gender: Gender;
-  occupation: string;
-  ssn: string;
+  latin?: string;
+}
+
+//this const object can be used both during compile time and run time
+
+
+export interface Patient extends NewPatientEntry {
+  id: string;
+ 
 }
 
 export type PatientEntry = Omit<Patient, "ssn">;
-export type NewPatientEntry = Omit<Patient, "id">;
+export type NewPatientEntry = z.infer<typeof NewEntrySchema>
