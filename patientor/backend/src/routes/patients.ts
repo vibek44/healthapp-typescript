b5f01c5a4b2cd687d7a76aad4d690b4165a7b9b3
vibek44 <kshetrivibek@gmail.com>
2026-07-15 10:14:52 +0300

    importing PatientEntry and Patient types and using it to type the request handler parameter req,res,next

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,17 +1,17 @@
-import express from "express";
+import express,  {type Response, type Request, type NextFunction }from "express";
 import patientsService from "../services/patientService.ts";
 import  { newEntryParser } from "../middleware/entryParser.ts";
 import errorHandler from "../middleware/errorHandler.ts";
+import type { PatientEntry, NewPatientEntry,Patient } from "../types.ts"; 
 // import type{ NewPatientEntry } from "../types.ts";
  
-
 const patientsRouter = express.Router();
 
-patientsRouter.get("/", (_req, res) => {
+patientsRouter.get("/", (_req, res:Response<PatientEntry[]>) => {
   res.send(patientsService.getPatientData());
 });
 
-patientsRouter.post("/",newEntryParser, (req, res,next) => {
+patientsRouter.post("/",newEntryParser, (req:Request<unknown,unknown,NewPatientEntry>, res:Response<Patient>, next:NextFunction) => {
   try {
     const newEntry = patientsService.addPatientData(req.body);
     res.send(newEntry);
@@ -20,5 +20,5 @@ patientsRouter.post("/",newEntryParser, (req, res,next) => {
   }
 });
 
- patientsRouter.use(errorHandler)
+patientsRouter.use(errorHandler)
 export default patientsRouter;
