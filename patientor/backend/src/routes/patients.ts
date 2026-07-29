d53a9aa9cc25633e9d9f1034b694bc1fb538f595
vibek44 <kshetrivibek@gmail.com>
2026-07-29 20:57:16 +0300

    adding get route handler  to  handle the individual patient data request and importing ErrorType interface to type the res object with union method

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,24 +1,50 @@
-import express,  {type Response, type Request, type NextFunction }from "express";
+import express, {
+  type Response,
+  type Request,
+  type NextFunction,
+} from "express";
 import patientsService from "../services/patientService.ts";
-import  { newEntryParser } from "../middleware/entryParser.ts";
+import { newEntryParser } from "../middleware/entryParser.ts";
 import errorHandler from "../middleware/errorHandler.ts";
-import type { PatientEntry, NewPatientEntry,Patient } from "../types.ts"; 
+import type {
+  PatientEntry,
+  NewPatientEntry,
+  Patient,
+  ErrorType,
+} from "../types.ts";
 // import type{ NewPatientEntry } from "../types.ts";
- 
+
 const patientsRouter = express.Router();
 
-patientsRouter.get("/", (_req, res:Response<PatientEntry[]>) => {
+patientsRouter.get("/", (_req, res: Response<PatientEntry[]>) => {
   res.send(patientsService.getPatientData());
 });
 
-patientsRouter.post("/",newEntryParser, (req:Request<unknown,unknown,NewPatientEntry>, res:Response<Patient>, next:NextFunction) => {
-  try {
-    const newEntry = patientsService.addPatientData(req.body);
-    res.send(newEntry);
-  } catch (error:unknown) {
-    next(error)
+patientsRouter.get("/:id", (req, res: Response<Patient | ErrorType>) => {
+  const individualData = patientsService.getSinglePatientData(req.params.id);
+  if (!individualData) {
+    res.status(404).send({ error: "Patient data not found" });
+    return;
   }
+  res.send(individualData);
 });
 
-patientsRouter.use(errorHandler)
+patientsRouter.post(
+  "/",
+  newEntryParser,
+  (
+    req: Request<unknown, unknown, NewPatientEntry>,
+    res: Response<Patient>,
+    next: NextFunction
+  ) => {
+    try {
+      const newEntry = patientsService.addPatientData(req.body);
+      res.send(newEntry);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
+);
+
+patientsRouter.use(errorHandler);
 export default patientsRouter;
