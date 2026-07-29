4b2b52f5e2d8710d0143eada3fcc6bae9f6c11e0
vibek44 <kshetrivibek@gmail.com>
2026-07-29 20:49:29 +0300

    adding interface object Entry which is empty and creating ErrorType Interface object to type the error and adding properties entries in Patient interface

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,32 +1,40 @@
-import { z } from "zod"
+import { z } from "zod";
 
-export const GenderObject={
-  Male:"male",
-  Female:"female",
-  Other:"other"
-} as const 
-// Gender  type is the union of all the propertie's value 
-export type Gender=typeof GenderObject[keyof typeof GenderObject]
+export const GenderObject = {
+  Male: "male",
+  Female: "female",
+  Other: "other",
+} as const;
+// Gender  type is the union of all the propertie's value
+export type Gender = (typeof GenderObject)[keyof typeof GenderObject];
 
-export const NewEntrySchema=z.object({
-  name:z.string().min(3),
-  occupation:z.string().min(2),
-  dateOfBirth:z.iso.date(),
-  gender:z.enum(Object.values(GenderObject)),
-  ssn:z.string().min(9).includes('-')
-})
+export const NewEntrySchema = z.object({
+  name: z.string().min(3),
+  occupation: z.string().min(2),
+  dateOfBirth: z.iso.date(),
+  gender: z.enum(Object.values(GenderObject)),
+  ssn: z.string().min(9).includes("-"),
+});
 
 export interface Diagnoses {
   code: string;
   name: string;
   latin?: string;
 }
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface Entry {}
 
-export type NewPatientEntry = z.infer<typeof NewEntrySchema>
+export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
+
+export type NewPatientEntry = z.infer<typeof NewEntrySchema>;
 //this const object can be used both during compile time and run time
 export interface Patient extends NewPatientEntry {
   id: string;
- 
+  entries: Entry[];
 }
 
-export type PatientEntry = Omit<Patient, "ssn">;
+export type PatientEntry = Omit<Patient, "ssn" | "entries">;
+
+export interface ErrorType {
+  error: string;
+}
