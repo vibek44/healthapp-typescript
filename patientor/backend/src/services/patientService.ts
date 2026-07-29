1e0aa3324e028be60a0411ee23ea0299b34202d2
vibek44 <kshetrivibek@gmail.com>
2026-07-29 20:59:46 +0300

    adding getSinglePatientData function helper to serve the found individual data if not undefined is returned

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,6 +1,7 @@
 import { v1 as uuid } from "uuid";
-import patientData from "../../data/patients.ts";
 import type { PatientEntry, Patient, NewPatientEntry } from "../types.ts";
+import patientData from "../../data/patients.ts";
+
 const data: Patient[] = patientData;
 const getPatientData = (): PatientEntry[] => {
   return data.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -12,9 +13,16 @@ const getPatientData = (): PatientEntry[] => {
   }));
 };
 
+const getSinglePatientData = (id: string): Patient | undefined => {
+  const data1 = data.find((el) => el.id === id);
+  if (!data1) return undefined;
+  else return data1;
+};
+
 const addPatientData = (entry: NewPatientEntry): Patient => {
   const newEntry = {
     id: uuid(),
+    entries: [],
     ...entry,
   };
   data.push(newEntry);
@@ -24,4 +32,5 @@ const addPatientData = (entry: NewPatientEntry): Patient => {
 export default {
   getPatientData,
   addPatientData,
+  getSinglePatientData,
 };
