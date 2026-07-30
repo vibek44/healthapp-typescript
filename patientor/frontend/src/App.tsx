346eab1cb69f9d4ccb912425507dbe5f884aeb18
vibek44 <kshetrivibek@gmail.com>
2026-07-30 21:14:17 +0300

    importing useMatch hook and using it to retrieve info from browser url  and adding client side routes in path  /patients/:id where component PatientInfoPage renders view of individual patient data

--- patientor/frontend/src/App.tsx
+++ patientor/frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { Route, Link, Routes, useMatch } from "react-router-dom";
+import { Button, Divider, Container, Typography } from "@mui/material";
 
 import { apiBaseUrl } from "./constants";
 import { Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
+import PatientInfoPage from "./components/PatientInfoPage";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
-
+  const [patient, setPatient] = useState<Patient | undefined>(undefined);
+  const match = useMatch("/patients/:id");
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
 
@@ -21,23 +23,44 @@ const App = () => {
     };
     void fetchPatientList();
   }, []);
-  
+  useEffect(() => {
+    if (match) {
+      const fetchPatientInfo = async () => {
+        if (match.params.id) {
+          console.log("match run eff1");
+          const patientDetail = await patientService.getIndividualPatientData(
+            match.params.id
+          );
+          setPatient(patientDetail);
+        }
+      };
+      void fetchPatientInfo();
+    }
+  }, [match?.params.id]);
   return (
     <div className="App">
-      <Router>
-        <Container>
-          <Typography variant="h3" sx={{ marginBottom: "0.5em" }}>
-            Patientor
-          </Typography>
-          <Button component={Link} to="/" variant="contained" color="primary">
-            Home
-          </Button>
-          <Divider sx={{ marginY: 2 }} />
-          <Routes>
-            <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
-          </Routes>
-        </Container>
-      </Router>
+      <Container>
+        <Typography variant="h3" sx={{ marginBottom: "0.5em" }}>
+          Patientor
+        </Typography>
+        <Button component={Link} to="/" variant="contained" color="primary">
+          Home
+        </Button>
+        <Divider sx={{ marginY: 2 }} />
+        <Routes>
+          <Route
+            path="/patients/:id"
+            element={<PatientInfoPage patient={patient} />}
+          />
+
+          <Route
+            path="/"
+            element={
+              <PatientListPage patients={patients} setPatients={setPatients} />
+            }
+          />
+        </Routes>
+      </Container>
     </div>
   );
 };
