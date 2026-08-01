a78c402cb7c3d801df5220e03591cce4f1fe07fb
vibek44 <kshetrivibek@gmail.com>
2026-08-01 20:53:47 +0300

    using patient data with proper patient entry

--- patientor/backend/data/patients.ts
+++ patientor/backend/data/patients.ts
@@ -1,5 +1,6 @@
 import type { Patient } from "../src/types.ts";
-const data: Patient[] = [
+
+const patients: Patient[] = [
   {
     id: "d2773336-f723-11e9-8f0b-362b9e155667",
     name: "John McClane",
@@ -7,16 +8,45 @@ const data: Patient[] = [
     ssn: "090786-122X",
     gender: "male",
     occupation: "New york city cop",
-    entries: [],
+    entries: [
+      {
+        id: "d811e46d-70b3-4d90-b090-4535c7cf8fb1",
+        date: "2015-01-02",
+        type: "Hospital",
+        specialist: "MD House",
+        diagnosisCodes: ["S62.5"],
+        description:
+          "Healing time appr. 2 weeks. patient doesn't remember how he got the injury.",
+        discharge: {
+          date: "2015-01-16",
+          criteria: "Thumb has healed.",
+        },
+      },
+    ],
   },
   {
     id: "d2773598-f723-11e9-8f0b-362b9e155667",
     name: "Martin Riggs",
     dateOfBirth: "1979-01-30",
-    ssn: "300179-77A",
+    ssn: "300179-777A",
     gender: "male",
     occupation: "Cop",
-    entries: [],
+    entries: [
+      {
+        id: "fcd59fa6-c4b4-4fec-ac4d-df4fe1f85f62",
+        date: "2019-08-05",
+        type: "OccupationalHealthcare",
+        specialist: "MD House",
+        employerName: "HyPD",
+        diagnosisCodes: ["Z57.1", "Z74.3", "M51.2"],
+        description:
+          "Patient mistakenly found himself in a nuclear plant waste site without protection gear. Very minor radiation poisoning. ",
+        sickLeave: {
+          startDate: "2019-08-05",
+          endDate: "2019-08-28",
+        },
+      },
+    ],
   },
   {
     id: "d27736ec-f723-11e9-8f0b-362b9e155667",
@@ -34,7 +64,33 @@ const data: Patient[] = [
     ssn: "050174-432N",
     gender: "female",
     occupation: "Forensic Pathologist",
-    entries: [],
+    entries: [
+      {
+        id: "b4f4eca1-2aa7-4b13-9a18-4a5535c3c8da",
+        date: "2019-10-20",
+        specialist: "MD House",
+        type: "HealthCheck",
+        description: "Yearly control visit. Cholesterol levels back to normal.",
+        healthCheckRating: 0,
+      },
+      {
+        id: "fcd59fa6-c4b4-4fec-ac4d-df4fe1f85f62",
+        date: "2019-09-10",
+        specialist: "MD House",
+        type: "OccupationalHealthcare",
+        employerName: "FBI",
+        description: "Prescriptions renewed.",
+      },
+      {
+        id: "37be178f-a432-4ba4-aac2-f86810e36a15",
+        date: "2018-10-05",
+        specialist: "MD House",
+        type: "HealthCheck",
+        description:
+          "Yearly control visit. Due to high cholesterol levels recommended to eat more vegetables.",
+        healthCheckRating: 1,
+      },
+    ],
   },
   {
     id: "d2773c6e-f723-11e9-8f0b-362b9e155667",
@@ -43,8 +99,17 @@ const data: Patient[] = [
     ssn: "090471-8890",
     gender: "male",
     occupation: "Digital evangelist",
-    entries: [],
+    entries: [
+      {
+        id: "54a8746e-34c4-4cf4-bf72-bfecd039be9a",
+        date: "2019-05-01",
+        specialist: "Dr Byte House",
+        type: "HealthCheck",
+        description: "Digital overdose, very bytestatic. Otherwise healthy.",
+        healthCheckRating: 0,
+      },
+    ],
   },
 ];
 
-export default data;
+export default patients;
