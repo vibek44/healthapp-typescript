9a52c1500270caaf8aac372ba464551346b507bc
vibek44 <kshetrivibek@gmail.com>
2026-08-01 21:10:08 +0300

    adding interface type HealthCheckEntry, OccupationalHealthcareEntry, HospitalEntry and union of this type is Entry which is used to type the properties entries of Patient interface

--- patientor/frontend/src/types.ts
+++ patientor/frontend/src/types.ts
@@ -7,8 +7,48 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
+
+interface Diagnoses {
+  code: string;
+  name: string;
+  latin?: string;
+}
+interface BaseEntry {
+  id: string;
+  date: string;
+  description: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnoses["code"]>;
+}
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+
+type HealthCheckRating =
+  (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: { date: string; criteria: string };
+}
+
+type Entry = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
 
 export interface Patient {
   id: string;
@@ -17,6 +57,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
