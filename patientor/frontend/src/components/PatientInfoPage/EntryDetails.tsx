4efcdfc6478a6f99f79ae756a5336f411a3ab40d
vibek44 <kshetrivibek@gmail.com>
2026-08-04 10:15:24 +0300

    creating EntryDetails functional component and importing EntryProps to type the props of this component, function body of component uses switch statement to load component based on their types and those imported components are OccupationalHealthcare, HospitalEntryDetails and HealthCheckEntry

---
+++ patientor/frontend/src/components/PatientInfoPage/EntryDetails.tsx
@@ -0,0 +1,23 @@
+import OccupationalHealthcare from "./OccupationalHealthcare";
+import type { EntryProps } from "../../types";
+import HospitalEntryDetails from "./HospitalEntry";
+import HealthCheckEntry from "./HealthCheckEntry";
+
+const assertNever = (entry: never): never => {
+  throw new Error(`unexpected value ${entry}`);
+};
+
+const EntryDetails = ({ entry, diagnoses }: EntryProps) => {
+  switch (entry.type) {
+    case "OccupationalHealthcare":
+      return <OccupationalHealthcare entry={entry} diagnoses={diagnoses} />;
+    case "Hospital":
+      return <HospitalEntryDetails entry={entry} diagnoses={diagnoses} />;
+    case "HealthCheck":
+      return <HealthCheckEntry entry={entry} diagnoses={diagnoses} />;
+    default:
+      assertNever(entry);
+  }
+};
+
+export default EntryDetails;
