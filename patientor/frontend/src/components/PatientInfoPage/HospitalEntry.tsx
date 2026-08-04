8a6a92f64aaa311cb1ec22807665a57c4e1d7f6c
vibek44 <kshetrivibek@gmail.com>
2026-08-04 10:25:30 +0300

    Defining  HospitalEntry  component to display entry of type HospitalEntry  and importing  HospitalEntryProps type to type the props received by this component and LocalHospitalIcon from mui is imported and use  to notify about  this type

---
+++ patientor/frontend/src/components/PatientInfoPage/HospitalEntry.tsx
@@ -0,0 +1,30 @@
+import type { HospitalEntryProps } from "../../types";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+
+const HospitalEntryDetails = ({ entry, diagnoses }: HospitalEntryProps) => {
+  return (
+    <div>
+      <p>
+        {entry.date}
+        <LocalHospitalIcon sx={{ verticalAlign: "middle" }} />
+      </p>
+      <p>{entry.description}</p>
+      <p>diagnose by: {entry.specialist}</p>
+      <ul>
+        {entry.diagnosisCodes?.map((el) => {
+          return diagnoses.map((diagnose) => {
+            if (diagnose.code === el) {
+              return (
+                <li key={el}>
+                  {el} {diagnose.name}
+                </li>
+              );
+            }
+          });
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default HospitalEntryDetails;
