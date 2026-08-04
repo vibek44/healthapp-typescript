f0f0cb19916e76a605ac3f6fc6f42ff1a41824d4
vibek44 <kshetrivibek@gmail.com>
2026-08-04 10:20:04 +0300

    Defining   OccupationalHealthcare component to display entry of type OccupationalHealthcare  and importing  OccupationalHealthcareEntryProps types to type the props received by this component and WorkIcon from mui to notify about  this type

---
+++ patientor/frontend/src/components/PatientInfoPage/OccupationalHealthcare.tsx
@@ -0,0 +1,34 @@
+import type { OccupationalHealthcareEntryProps } from "../../types";
+import WorkIcon from "@mui/icons-material/Work";
+
+const OccupationalHealthcare = ({
+  entry,
+  diagnoses,
+}: OccupationalHealthcareEntryProps) => {
+  return (
+    <div>
+      <p>
+        {entry.date} <WorkIcon sx={{ verticalAlign: "middle" }} />{" "}
+        {entry.employerName}
+      </p>
+      <p>{entry.description}</p>
+      <p>diagnose by: {entry.specialist}</p>
+      {}
+      <ul>
+        {entry.diagnosisCodes?.map((el) => {
+          return diagnoses.map((diagnose) => {
+            if (diagnose.code === el) {
+              return (
+                <li key={el}>
+                  {el} {diagnose.name}
+                </li>
+              );
+            }
+          });
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default OccupationalHealthcare;
