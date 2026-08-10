3b8f317e68d202e9e723cb843bb9a3eeb98a0278
vibek44 <kshetrivibek@gmail.com>
2026-08-10 21:12:42 +0300

    using zod object to create BaseEntry and creating other entry HospitalEntry,HealthCheckEntry, OccupationalHealthcareEntry by extending BaseEntry and finally crating EntrySchema which is discriminatedUnion of previous entries and also creating entry types using zod infer

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import z from "zod";
 
 export const Gender = {
   Male: "male",
@@ -14,73 +14,57 @@ export const PatientEntrySchema = z.object({
   ssn: z.string().regex(/^\d{6}-\d{3}[A-Za-z]$/, "invalid ssn format"),
 });
 
-export interface Diagnoses {
-  code: string;
-  name: string;
-  latin?: string;
-}
-interface BaseEntry {
-  id: string;
-  date: string;
-  description: string;
-  specialist: string;
-  diagnosisCodes?: Array<Diagnoses["code"]>;
-}
+export const Diagnoses = z.object({
+  code: z.string(),
+  name: z.string(),
+  latin: z.string().optional(),
+});
 
-export const HealthCheckRating = {
-  Healthy: 0,
-  LowRisk: 1,
-  HighRisk: 2,
-  CriticalRisk: 3,
-} as const;
+export type Diagnoses = z.infer<typeof Diagnoses>;
 
-export type HealthCheckRating =
-  (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+const BaseEntrySchema = z.object({
+  id: z.string(),
+  date: z.iso.date(),
+  description: z.string(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(Diagnoses.shape.code).optional(),
+});
 
-interface HealthCheckEntry extends BaseEntry {
-  type: "HealthCheck";
-  healthCheckRating: HealthCheckRating;
-}
-export type HealthCheckEntryWithoutId = Omit<HealthCheckEntry, "id">;
+const HealthCheckRatingSchema = z.union([
+  z.literal(0),
+  z.literal(1),
+  z.literal(2),
+  z.literal(3),
+]);
+type HealthCheckRating = z.infer<typeof HealthCheckRatingSchema>;
 
-interface OccupationalHealthcareEntry extends BaseEntry {
-  type: "OccupationalHealthcare";
-  employerName: string;
-  sickLeave?: { startDate: string; endDate: string };
-}
-export type OccupationalEntryWithoutId = Omit<
-  OccupationalHealthcareEntry,
-  "id"
+const HealthCheckEntrySchema = BaseEntrySchema.extend({
+  type: z.literal("HealthCheck"),
+  healthCheckRating: HealthCheckRatingSchema,
+});
+const HealthCheckNoId = HealthCheckEntrySchema.omit({ id: true });
+type HealthCheckEntry = z.infer<typeof HealthCheckEntrySchema>;
+type HealthCheckNoId = z.infer<typeof HealthCheckNoId>;
+
+const OccupationalHealthcareEntrySchema = BaseEntrySchema.extend({
+  type: z.literal("OccupationalHealthcare"),
+  employerName: z.string(),
+  sickLeave: z
+    .object({ startDate: z.string(), endDate: z.string() })
+    .optional(),
+});
+type OccupationalHealthcareEntry = z.infer<
+  typeof OccupationalHealthcareEntrySchema
 >;
+const HospitalEntrySchema = BaseEntrySchema.extend({
+  type: z.literal("Hospital"),
+  discharge: z.object({ date: z.string(), criteria: z.string() }),
+});
+type HospitalEntry = z.infer<typeof HospitalEntrySchema>;
 
-interface HospitalEntry extends BaseEntry {
-  type: "Hospital";
-  discharge: { date: string; criteria: string };
-}
-export type HospitalEntryWithoutId = Omit<HospitalEntry, "id">;
-
-export type Entry =
-  | HealthCheckEntry
-  | OccupationalHealthcareEntry
-  | HospitalEntry;
-type UnionOmit<T, K extends string | number | symbol> = T extends unknown
-  ? Omit<T, K>
-  : never;
-
-export type EntryWithoutId = UnionOmit<Entry, "id">;
-
-export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
-
-export type NewPatientEntry = z.infer<typeof PatientEntrySchema>;
-//this const object can be used both during compile time and run time
-
-export interface Patient extends NewPatientEntry {
-  id: string;
-  entries: Entry[];
-}
-
-export type PatientEntry = Omit<Patient, "ssn" | "entries">;
-
-export interface ErrorType {
-  error: string;
-}
+const EntrySchema = z.discriminatedUnion("type", [
+  HospitalEntrySchema,
+  HealthCheckEntrySchema,
+  OccupationalHealthcareEntrySchema,
+]);
+type Entry = z.infer<typeof EntrySchema>;
