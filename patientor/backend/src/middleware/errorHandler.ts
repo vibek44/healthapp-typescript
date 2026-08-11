a982b72f72983e6da2e13e0f6b8c3bdfc1ef74d1
vibek44 <kshetrivibek@gmail.com>
2026-08-11 21:11:27 +0300

    configuring error properties to have value of  message properties of error object

--- patientor/backend/src/middleware/errorHandler.ts
+++ patientor/backend/src/middleware/errorHandler.ts
@@ -1,15 +1,18 @@
-import type { Request,Response,NextFunction } from "express"
-import z from "zod"
-const errorHandler = (error:unknown, _req:Request, res:Response, next:NextFunction) => {
-    if(error instanceof z.ZodError){
-        res.status(400).send({error:error.issues})
-    }
-    else if(error instanceof Error){
-       res.status(400).send({error})
-    }
-    else{
-        next(error)
-    }
-}
+import type { Request, Response, NextFunction } from "express";
+import z from "zod";
+const errorHandler = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else if (error instanceof Error) {
+    res.status(400).send({ error: error.message });
+  } else {
+    next(error);
+  }
+};
 
-export default errorHandler
+export default errorHandler;
