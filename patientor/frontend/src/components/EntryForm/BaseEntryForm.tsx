92bee58b92622726d673bb1177ecbcdb80dd6c71
vibek44 <kshetrivibek@gmail.com>
2026-08-17 21:18:41 +0300

    Extracting logic related to basic  form into BaseEntryForm component and  moving the logic of entry form logic user uses to create HealthCheck,Hospital and Occupational entry in this component

---
+++ patientor/frontend/src/components/EntryForm/BaseEntryForm.tsx
@@ -0,0 +1,108 @@
+import {
+  TextField,
+  Grid,
+  Button,
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+} from "@mui/material";
+import { useState } from "react";
+import { HealthCheckRating } from "../../types";
+
+interface BaseEntryProps {
+  entryType: string;
+  handleVisibility: () => void;
+}
+const BaseEntryForm = ({ entryType, handleVisibility }: BaseEntryProps) => {
+  if (!entryType) return null;
+  const [rating, setRating] = useState<number>(0);
+  return (
+    <>
+      <TextField
+        required
+        size="small"
+        label="Date"
+        type="date"
+        slotProps={{
+          inputLabel: {
+            shrink: true,
+          },
+        }}
+      />
+      <TextField required size="small" label="Description" />
+      <TextField required size="small" label="Specialist" />
+      <TextField size="small" label="Diagnosis Codes(comma separated)" />
+
+      {entryType === "Hospital" && (
+        <>
+          <TextField
+            required
+            type="date"
+            label="Discharge date"
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
+          <TextField required type="text" label="criteria" />
+        </>
+      )}
+      {entryType === "HealthCheck" && (
+        <FormControl fullWidth>
+          <InputLabel>Health rating</InputLabel>
+          <Select
+            value={rating}
+            onChange={({ target }) => setRating(target.value)}
+          >
+            {Object.entries(HealthCheckRating).map((el) => (
+              <MenuItem value={el[1]}>
+                {el[1]}-{el[0]}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )}
+      {entryType === "OccupationalHealthcare" && (
+        <>
+          <TextField required type="text" label="Employer name" />
+          <TextField
+            type="date"
+            label="Leave Startdate"
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
+          <TextField
+            type="date"
+            label="Leave EndDate"
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
+        </>
+      )}
+
+      <Grid container justifyContent="space-between">
+        <Button variant="contained" type="submit">
+          SUBMIT
+        </Button>
+        <Button
+          onClick={handleVisibility} //handleVisibility
+          variant="contained"
+          color="secondary"
+          type="button"
+        >
+          CANCEL
+        </Button>
+      </Grid>
+    </>
+  );
+};
+
+export default BaseEntryForm;
