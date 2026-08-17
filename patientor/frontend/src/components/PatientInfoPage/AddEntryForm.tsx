1be3bf952500006820397f00512a92510574e8ba
vibek44 <kshetrivibek@gmail.com>
2026-08-17 10:24:33 +0300

    Adding logic to display Hospital,HealthCheck,OccupationalHealthcare form based on entryType state variable and users selecting create  entry in userInterface

--- patientor/frontend/src/components/PatientInfoPage/AddEntryForm.tsx
+++ patientor/frontend/src/components/PatientInfoPage/AddEntryForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 //import BaseEntryForm from "./BaseEntryForm";
+import { HealthCheckRating } from "../../types";
 import {
   Button,
   Grid,
@@ -17,67 +18,122 @@ interface Props {
 
 const AddEntryForm = ({ handleVisibility }: Props) => {
   const [entryType, setEntryType] = useState<string>("");
+  const [rating, setRating] = useState<number>(0);
   const handleSetEntryType = (value: string) => setEntryType(value);
   console.log(entryType);
   return (
-    <div>
-      <form
-        style={{
-          display: "grid",
-          gap: "1em",
-          border: "2px dashed ",
-          padding: "3em",
-        }}
-      >
-        <Typography variant="h5">New Entry Form</Typography>
-        <Divider sx={{ marginY: "1em" }} />
-        <FormControl fullWidth>
-          <InputLabel>select entry</InputLabel>
-          <Select
-            value={entryType}
-            onChange={({ target }) => handleSetEntryType(target.value)}
-          >
-            <MenuItem value="HealthCheck">HelathCheck </MenuItem>
-            <MenuItem value="Hospital">Hospital </MenuItem>
-            <MenuItem value="OccupationalHealthcare">
-              OccupationalHealthcare
-            </MenuItem>
-          </Select>
-        </FormControl>
+    <form
+      style={{
+        display: "grid",
+        gap: "1em",
+        border: "2px dashed ",
+        padding: "3em",
+      }}
+    >
+      <Typography variant="h5">New Entry Form</Typography>
+      <Divider sx={{ marginY: "1em" }} />
+      <FormControl fullWidth>
+        <InputLabel>select entry</InputLabel>
+        <Select
+          value={entryType}
+          onChange={({ target }) => handleSetEntryType(target.value)}
+        >
+          <MenuItem value="HealthCheck">HelathCheck </MenuItem>
+          <MenuItem value="Hospital">Hospital </MenuItem>
+          <MenuItem value="OccupationalHealthcare">
+            OccupationalHealthcare
+          </MenuItem>
+        </Select>
+      </FormControl>
 
-        {entryType && (
-          <>
-            <TextField
-              required
-              size="small"
-              label="Date"
-              type="date"
-              slotProps={{
-                inputLabel: {
-                  shrink: true,
-                },
-              }}
-            />
-            <TextField required size="small" label="Description" />
-            <TextField required size="small" label="Specialist" />
-            <TextField size="small" label="Diagnosis Codes(comma separated)" />
-            <Grid container justifyContent="space-between">
-              <Button variant="contained" type="submit">
-                SUBMIT
-              </Button>
-              <Button
-                onClick={handleVisibility}
-                variant="contained"
-                color="secondary"
-                type="button"
-              >
-                CANCEL
-              </Button>
-            </Grid>
-          </>
-        )}
-      </form>
-    </div>
+      {entryType && (
+        <>
+          <TextField
+            required
+            size="small"
+            label="Date"
+            type="date"
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
+          <TextField required size="small" label="Description" />
+          <TextField required size="small" label="Specialist" />
+          <TextField size="small" label="Diagnosis Codes(comma separated)" />
+          {entryType === "HealthCheck" && (
+            <>
+              <FormControl fullWidth>
+                <InputLabel>Health rating</InputLabel>
+                <Select
+                  value={rating}
+                  onChange={({ target }) => setRating(target.value)}
+                >
+                  {Object.entries(HealthCheckRating).map((el) => (
+                    <MenuItem value={el[1]}>
+                      {el[1]}-{el[0]}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </>
+          )}
+          {entryType === "Hospital" && (
+            <>
+              <TextField
+                required
+                type="date"
+                label="Discharge date"
+                slotProps={{
+                  inputLabel: {
+                    shrink: true,
+                  },
+                }}
+              />
+              <TextField required type="text" label="criteria" />
+            </>
+          )}
+          {entryType === "OccupationalHealthcare" && (
+            <>
+              <TextField required type="text" label="Employer name" />
+              <TextField
+                type="date"
+                label="Leave Startdate"
+                slotProps={{
+                  inputLabel: {
+                    shrink: true,
+                  },
+                }}
+              />
+              <TextField
+                type="date"
+                label="Leave EndDate"
+                slotProps={{
+                  inputLabel: {
+                    shrink: true,
+                  },
+                }}
+              />
+            </>
+          )}
+
+          <Grid container justifyContent="space-between">
+            <Button variant="contained" type="submit">
+              SUBMIT
+            </Button>
+            <Button
+              onClick={handleVisibility}
+              variant="contained"
+              color="secondary"
+              type="button"
+            >
+              CANCEL
+            </Button>
+          </Grid>
+        </>
+      )}
+    </form>
   );
 };
 
