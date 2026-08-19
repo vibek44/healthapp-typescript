950ef781be7898c06a03b9c161355edd99332726
vibek44 <kshetrivibek@gmail.com>
2026-08-19 11:54:23 +0300

    declaring helper function to return defaultValues used bt useForm hook based on entryType value

---
+++ patientor/frontend/src/utils.tsx
@@ -0,0 +1,29 @@
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled entry case ${value}`);
+};
+export const getDefaultValues = (entryType: unknown) => {
+  const base = {
+    type: entryType,
+    date: "",
+    description: "",
+    specialist: "",
+    diagnosisCodes: [] as string[],
+  };
+  if (typeof entryType !== "string") throw new Error("not string type");
+  if (
+    entryType === "Hospital" ||
+    entryType === "HealthCheck" ||
+    entryType === "OccupationalHealthcare"
+  ) {
+    switch (entryType) {
+      case "HealthCheck":
+        return { ...base, healthCheckRating: 0 };
+      case "Hospital":
+        return { ...base, dischargeDate: "", criteria: "" };
+      case "OccupationalHealthcare":
+        return { ...base, employerName: "", startDate: "", endDate: "" };
+      default:
+        return assertNever(entryType);
+    }
+  }
+};
