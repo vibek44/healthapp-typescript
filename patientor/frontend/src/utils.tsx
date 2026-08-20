8491eced19b548c60ecb1ba46ed5e2e6e6209e24
vibek44 <kshetrivibek@gmail.com>
2026-08-20 19:21:45 +0300

    improving typeguard code for entryType to narrow the entryType type

--- patientor/frontend/src/utils.tsx
+++ patientor/frontend/src/utils.tsx
@@ -1,7 +1,16 @@
+import type { EntryFormValues } from "./types";
+
 const assertNever = (value: never): never => {
-  throw new Error(`Unhandled entry case ${value}`);
+  throw new Error(`unhandled Case ${value}`);
 };
 export const getDefaultValues = (entryType: unknown) => {
+  if (typeof entryType !== "string") throw new Error("not string type");
+  if (
+    entryType !== "HealthCheck" &&
+    entryType !== "Hospital" &&
+    entryType !== "OccupationalHealthcare"
+  )
+    throw new Error("not entry type");
   const base = {
     type: entryType,
     date: "",
@@ -9,21 +18,15 @@ export const getDefaultValues = (entryType: unknown) => {
     specialist: "",
     diagnosisCodes: "",
   };
-  if (typeof entryType !== "string") throw new Error("not string type");
-  if (
-    entryType === "Hospital" ||
-    entryType === "HealthCheck" ||
-    entryType === "OccupationalHealthcare"
-  ) {
-    switch (entryType) {
-      case "HealthCheck":
-        return { ...base, healthCheckRating: 0 };
-      case "Hospital":
-        return { ...base, dischargeDate: "", criteria: "" };
-      case "OccupationalHealthcare":
-        return { ...base, employerName: "", startDate: "", endDate: "" };
-      default:
-        return assertNever(entryType);
-    }
+
+  switch (entryType) {
+    case "Hospital":
+      return { ...base, dischargeDate: "", criteria: "" };
+    case "HealthCheck":
+      return { ...base, healthCheckRating: 0 };
+    case "OccupationalHealthcare":
+      return { ...base, employerName: "", startDate: "", endDate: "" };
+    default:
+      return assertNever(entryType);
   }
 };
