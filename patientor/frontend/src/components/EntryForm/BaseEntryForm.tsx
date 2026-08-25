f0594633c276d5a2756247fe24e24cac4aa31feb
vibek44 <kshetrivibek@gmail.com>
2026-08-25 10:16:09 +0300

    Adding error and helperText props to TextField component to  handle error for respective fields

--- patientor/frontend/src/components/EntryForm/BaseEntryForm.tsx
+++ patientor/frontend/src/components/EntryForm/BaseEntryForm.tsx
@@ -1,20 +1,14 @@
-import { TextField, MenuItem, Grid, Button } from "@mui/material";
+import { TextField, MenuItem } from "@mui/material";
 import { Controller, Control, FieldErrors } from "react-hook-form";
 import type { EntryFormValues } from "../../types";
 import { HealthCheckRating } from "../../types";
 
 interface BaseEntryProps {
   entryType: string;
-  handleVisibility: () => void;
   control: Control<EntryFormValues>;
   errors: FieldErrors<EntryFormValues>;
 }
-const BaseEntryForm = ({
-  entryType,
-  handleVisibility,
-  control,
-  errors,
-}: BaseEntryProps) => {
+const BaseEntryForm = ({ entryType, control, errors }: BaseEntryProps) => {
   if (!entryType) return null;
 
   return (
@@ -96,6 +90,8 @@ const BaseEntryForm = ({
             type="text"
             label="diagnosisCodes"
             slotProps={{ inputLabel: { shrink: true } }}
+            error={!!errors?.specialist}
+            helperText={errors?.specialist?.message}
           />
         )}
       />
@@ -111,6 +107,8 @@ const BaseEntryForm = ({
               label="rating"
               type="text"
               value={field.value ?? 0}
+              error={!!errors?.specialist}
+              helperText={errors?.specialist?.message}
             >
               {Object.entries(HealthCheckRating).map(([key, value]) => (
                 <MenuItem key={value} value={value}>
@@ -135,6 +133,8 @@ const BaseEntryForm = ({
                 type="date"
                 label="dischargeDate"
                 slotProps={{ inputLabel: { shrink: true } }}
+                error={!!errors?.specialist}
+                helperText={errors?.specialist?.message}
               />
             )}
           />
@@ -149,6 +149,8 @@ const BaseEntryForm = ({
                 type="text"
                 label="criteria"
                 slotProps={{ inputLabel: { shrink: true } }}
+                error={!!errors?.specialist}
+                helperText={errors?.specialist?.message}
               />
             )}
           />
@@ -167,6 +169,8 @@ const BaseEntryForm = ({
                 type="text"
                 label="Employer-Name"
                 slotProps={{ inputLabel: { shrink: true } }}
+                error={!!errors?.specialist}
+                helperText={errors?.specialist?.message}
               />
             )}
           />
@@ -203,140 +207,3 @@ const BaseEntryForm = ({
 };
 
 export default BaseEntryForm;
-
-/*
-  const [rating, setRating] = useState<number>(0);
-  const [date, setDate] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [specialist, setSpecialist] = useState<string>("");
-  const [diagnosisCodes, setDiagnosisCodes] = useState<string[]>([]);
-  const [employerName, setEmployerName] = useState<string>("");
-  const [sickLeave, setSickLeave] = useState<{
-    startDate: string;
-    endDate: string;
-  }>({ startDate: "", endDate: "" });
-  const [discharge, setDischarge] = useState<{
-    date: string;
-    criteria: string;
-  }>({
-    date: "",
-    criteria: "",
-  });
-
-  console.log(diagnosisCodes, date);
-
-  return (
-    <>
-      <TextField
-        required
-        size="small"
-        label="Date"
-        type="date"
-        value={date}
-        onChange={({ target }) => setDate(target.value)}
-        slotProps={{
-          inputLabel: {
-            shrink: true,
-          },
-        }}
-      />
-      <TextField
-        required
-        size="small"
-        type="text"
-        label="Description"
-        value={description}
-        onChange={({ target }) => setDescription(target.value)}
-      />
-      <TextField
-        required
-        size="small"
-        type="text"
-        label="Specialist"
-        value={specialist}
-        onChange={({ target }) => setSpecialist(target.value)}
-      />
-      <TextField
-        size="small"
-        placeholder="L20, Z74.3"
-        label="Diagnosis Codes(comma separated)"
-        value={diagnosisCodes}
-        onChange={({ target }) => setDiagnosisCodes(target.value.split(","))}
-        slotProps={{
-          inputLabel: {
-            shrink: true,
-          },
-        }}
-      />
-
-      {entryType === "Hospital" && (
-        <>
-          <TextField
-            required
-            type="date"
-            label="Discharge date"
-            slotProps={{
-              inputLabel: {
-                shrink: true,
-              },
-            }}
-          />
-          <TextField required type="text" label="criteria" />
-        </>
-      )}
-      {entryType === "HealthCheck" && (
-        <FormControl fullWidth>
-          <InputLabel>Health rating</InputLabel>
-          <Select
-            value={rating}
-            onChange={({ target }) => setRating(target.value)}
-          >
-            {Object.entries(HealthCheckRating).map((el) => (
-              <MenuItem value={el[1]}>
-                {el[1]}-{el[0]}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      )}
-      {entryType === "OccupationalHealthcare" && (
-        <>
-          <TextField required type="text" label="Employer name" />
-          <TextField
-            type="date"
-            label="Leave Startdate"
-            slotProps={{
-              inputLabel: {
-                shrink: true,
-              },
-            }}
-          />
-          <TextField
-            type="date"
-            label="Leave EndDate"
-            slotProps={{
-              inputLabel: {
-                shrink: true,
-              },
-            }}
-          />
-        </>
-      )}
-
-      <Grid container justifyContent="space-between">
-        <Button variant="contained" type="submit">
-          SUBMIT
-        </Button>
-        <Button
-          onClick={handleVisibility} //handleVisibility
-          variant="contained"
-          color="secondary"
-          type="button"
-        >
-          CANCEL
-        </Button>
-      </Grid>
-    </>
-  );
-
-  */
