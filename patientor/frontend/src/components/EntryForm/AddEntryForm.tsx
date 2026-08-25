3e9817dcd73de55450ea4e3b64a3f97c2e3dc85e
vibek44 <kshetrivibek@gmail.com>
2026-08-25 10:05:24 +0300

    calling useForm hook in AddEntryForm insted of BaseEntryForm and destructuring watch,control,handleSubmit returned by useForm hook, watch is used to subscribe the value of type which is name of Controller of input field type

--- patientor/frontend/src/components/EntryForm/AddEntryForm.tsx
+++ patientor/frontend/src/components/EntryForm/AddEntryForm.tsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import BaseEntryForm from "./BaseEntryForm";
+import type { EntryFormValues } from "../../types";
 
 import {
   Typography,
   Divider,
-  FormControl,
-  InputLabel,
-  Select,
   MenuItem,
+  TextField,
+  Grid,
+  Button,
 } from "@mui/material";
 interface Props {
   handleVisibility: () => void;
 }
 
 const AddEntryForm = ({ handleVisibility }: Props) => {
-  const [entryType, setEntryType] = useState<string>("");
-  const handleSetEntryType = (value: string) => setEntryType(value);
-  //console.log(entryType);
+  const {
+    control,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EntryFormValues>();
+
+  const entryType = watch("type");
+
+  // console.log(for);
+  // const formValues = watch();
+  //console.log(formValues, entryType);
+
+  const onSubmit = (data: EntryFormValues) => console.log(data);
+  console.log(entryType);
   return (
     <form
+      onSubmit={handleSubmit(onSubmit)}
       style={{
         display: "grid",
         gap: "1em",
@@ -34,24 +48,43 @@ const AddEntryForm = ({ handleVisibility }: Props) => {
         </Typography>
       )}
       <Divider sx={{ marginY: "1em" }} />
-      <FormControl fullWidth>
-        <InputLabel>select entry</InputLabel>
-        <Select
-          value={entryType}
-          onChange={({ target }) => handleSetEntryType(target.value)}
-        >
-          <MenuItem value="HealthCheck">HelathCheck </MenuItem>
-          <MenuItem value="Hospital">Hospital </MenuItem>
-          <MenuItem value="OccupationalHealthcare">
-            OccupationalHealthcare
-          </MenuItem>
-        </Select>
-      </FormControl>
+      <Controller
+        name="type"
+        defaultValue=""
+        control={control}
+        render={({ field }) => (
+          <TextField {...field} select label="Select Entry-Type">
+            <MenuItem value="">Select Entry type </MenuItem>
+            <MenuItem value="HealthCheck">HelathCheck </MenuItem>
+            <MenuItem value="Hospital">Hospital </MenuItem>
+            <MenuItem value="OccupationalHealthcare">
+              OccupationalHealthcare
+            </MenuItem>
+          </TextField>
+        )}
+      />
+
       {entryType && (
         <BaseEntryForm
           entryType={entryType}
-          handleVisibility={handleVisibility}
+          control={control}
+          errors={errors}
         />
+      )}
+      {entryType && (
+        <Grid container justifyContent="space-between">
+          <Button variant="contained" type="submit">
+            SUBMIT
+          </Button>
+          <Button
+            onClick={handleVisibility} //handleVisibility
+            variant="contained"
+            color="secondary"
+            type="button"
+          >
+            CANCEL
+          </Button>
+        </Grid>
       )}
     </form>
   );
