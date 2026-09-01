6435e25e4a30560a208457a82dbb8646e554ebb1
vibek44 <kshetrivibek@gmail.com>
2026-09-01 10:41:16 +0300

    removing defaultValue from all the Controller controlling form field as default values are provided by useForm and reset from reacthook form

--- patientor/frontend/src/components/EntryForm/BaseEntryForm.tsx
+++ patientor/frontend/src/components/EntryForm/BaseEntryForm.tsx
@@ -26,7 +26,6 @@ const BaseEntryForm = ({
     <>
       <Controller
         name="date"
-        defaultValue=""
         control={control}
         rules={{ required: "Date is required" }}
         render={({ field }) => (
@@ -42,18 +41,40 @@ const BaseEntryForm = ({
         )}
       />
       <Controller
+        name="specialist"
+        control={control}
+        rules={{
+          required: "specialist is required",
+          minLength: {
+            value: 3,
+            message: "specialist should be atleast 3 character long",
+          },
+          maxLength: { value: 20, message: "maximum 20 character" },
+        }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            required
+            type="text"
+            label="Specialist"
+            slotProps={{ inputLabel: { shrink: true } }}
+            error={!!errors?.specialist}
+            helperText={errors?.specialist?.message}
+          />
+        )}
+      />
+      <Controller
         name="description"
-        defaultValue=""
         control={control}
         rules={{
           required: "Description is required",
           minLength: {
             value: 6,
-            message: "description needs to be atleast 6 character long",
+            message: "needs to be atleast 6 character long",
           },
           maxLength: {
-            value: 30,
-            message: "description cannot  exceed 30 charactor",
+            value: 60,
+            message: "cannot  exceed 60 charactor",
           },
         }}
         render={({ field }) => (
@@ -68,34 +89,9 @@ const BaseEntryForm = ({
           />
         )}
       />
-      <Controller
-        name="specialist"
-        defaultValue=""
-        control={control}
-        rules={{
-          required: "specialist is required",
-          minLength: {
-            value: 3,
-            message: "specialist should be atleast 3 character long",
-          },
-          maxLength: { value: 20, message: "maximum 30 character" },
-        }}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            required
-            type="text"
-            label="Specialist"
-            slotProps={{ inputLabel: { shrink: true } }}
-            error={!!errors?.specialist}
-            helperText={errors?.specialist?.message}
-          />
-        )}
-      />
 
       <Controller
         name="diagnosisCodes"
-        defaultValue={[]}
         control={control}
         render={({ field: { onChange, value, ...field } }) => (
           <Autocomplete
@@ -125,7 +121,6 @@ const BaseEntryForm = ({
           return (
             <Controller
               name="healthCheckRating"
-              defaultValue={0}
               control={control}
               render={({ field }) => (
                 <TextField
@@ -154,7 +149,6 @@ const BaseEntryForm = ({
             <>
               <Controller
                 name="dischargeDate"
-                defaultValue=""
                 control={control}
                 rules={{ required: "DischargeDate is missing" }}
                 render={({ field }) => (
@@ -171,7 +165,6 @@ const BaseEntryForm = ({
               />
               <Controller
                 name="criteria"
-                defaultValue=""
                 control={control}
                 rules={{
                   required: "criteria is missing",
@@ -206,7 +199,6 @@ const BaseEntryForm = ({
             <>
               <Controller
                 name="employerName"
-                defaultValue=""
                 control={control}
                 rules={{ required: "Employername is missing" }}
                 render={({ field }) => (
@@ -222,7 +214,6 @@ const BaseEntryForm = ({
               />
               <Controller
                 name="startDate"
-                defaultValue=""
                 control={control}
                 render={({ field }) => (
                   <TextField
@@ -254,137 +245,3 @@ const BaseEntryForm = ({
 };
 
 export default BaseEntryForm;
-
-/*
-{entryType === "HealthCheck" &&
-        (() => {
-          const healthCheckErrors =
-            errors as FieldErrors<HealthCheckFormValues>;
-          return (
-            <Controller
-              name="healthCheckRating"
-              defaultValue={0}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  select
-                  label="rating"
-                  type="text"
-                  value={field.value ?? 0}
-                  error={!!healthCheckErrors?.healthCheckRating}
-                  helperText={healthCheckErrors?.healthCheckRating?.message}
-                >
-                  {Object.entries(HealthCheckRating).map(([key, value]) => (
-                    <MenuItem key={value} value={value}>
-                      {key} - {value}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              )}
-            />
-          );
-        })()}
-      {entryType === "Hospital" &&
-        (() => {
-          const hospitalErrors = errors as FieldErrors<HospitalFormValues>;
-          return (
-            <>
-              <Controller
-                name="dischargeDate"
-                defaultValue=""
-                control={control}
-                rules={{ required: "DischargeDate is missing" }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    required
-                    type="date"
-                    label="dischargeDate"
-                    slotProps={{ inputLabel: { shrink: true } }}
-                    error={!!hospitalErrors?.dischargeDate}
-                    helperText={hospitalErrors?.dischargeDate?.message}
-                  />
-                )}
-              />
-              <Controller
-                name="criteria"
-                defaultValue=""
-                control={control}
-                rules={{
-                  required: "criteria is missing",
-                  validate: (val) => {
-                    if (val)
-                      return (
-                        val.length > 10 ||
-                        "criteria field needs proper explanation"
-                      );
-                  },
-                }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    required
-                    type="text"
-                    label="criteria"
-                    slotProps={{ inputLabel: { shrink: true } }}
-                    error={!!hospitalErrors?.criteria}
-                    helperText={hospitalErrors?.criteria?.message}
-                  />
-                )}
-              />
-            </>
-          );
-        })()}
-      {entryType === "OccupationalHealthcare" &&
-        (() => {
-          const occupationalErrors =
-            errors as FieldErrors<OccupationalHealthcareFormValues>;
-          return (
-            <>
-              <Controller
-                name="employerName"
-                defaultValue=""
-                control={control}
-                rules={{ required: "Employername is missing" }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    required
-                    type="text"
-                    label="Employer-Name"
-                    slotProps={{ inputLabel: { shrink: true } }}
-                    error={!!occupationalErrors?.employerName}
-                  />
-                )}
-              />
-              <Controller
-                name="startDate"
-                defaultValue=""
-                control={control}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    type="date"
-                    label="StartDate"
-                    slotProps={{ inputLabel: { shrink: true } }}
-                  />
-                )}
-              />
-              <Controller
-                name="endDate"
-                defaultValue=""
-                control={control}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    type="date"
-                    label="EndDate"
-                    slotProps={{ inputLabel: { shrink: true } }}
-                  />
-                )}
-              />
-            </>
-          );
-        })()}
-*/
