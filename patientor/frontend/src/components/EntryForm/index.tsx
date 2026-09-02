e701c9f6a4c7415813008094769976d9f1f85354
vibek44 <kshetrivibek@gmail.com>
2026-09-02 10:09:27 +0300

    adding new file which export AddEntryModal component which uses modal(Dialog) to display the Entry Form and disappear when cancel button is clicked, the props are destructured and typed

---
+++ patientor/frontend/src/components/EntryForm/index.tsx
@@ -0,0 +1,33 @@
+import { Dialog, DialogTitle, Divider } from "@mui/material";
+import AddEntryForm from "./AddEntryForm";
+import type { Patient, Diagnoses } from "../../types";
+interface AddEntryProps {
+  open: boolean;
+  onClose: () => void;
+  patient: Patient;
+  setPatient: React.Dispatch<React.SetStateAction<Patient | undefined>>;
+  diagnoses: Diagnoses[];
+}
+
+const AddEntryModal = ({
+  open,
+  onClose,
+  patient,
+  setPatient,
+  diagnoses,
+}: AddEntryProps) => {
+  return (
+    <Dialog fullWidth={true} maxWidth="md" open={open}>
+      <DialogTitle sx={{ marginX: "auto" }}>New Entry Form</DialogTitle>
+      <Divider sx={{ marginTop: "0.5em" }} />
+      <AddEntryForm
+        onClose={onClose}
+        patient={patient}
+        setPatient={setPatient}
+        diagnoses={diagnoses}
+      />
+    </Dialog>
+  );
+};
+
+export default AddEntryModal;
