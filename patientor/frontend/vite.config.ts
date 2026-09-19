034cca8e53ecf4241708f393b1f1f0e6ca5d1148
vibek44 <kshetrivibek@gmail.com>
2026-09-19 21:16:18 +0300

    adding vite configuration properties drop to remove console logging and debugging related code during compilation

--- patientor/frontend/vite.config.ts
+++ patientor/frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  esbuild: {
+    //@ts-ignore
+    drop: ["console", "debugger"],
+  },
   server: {
     proxy: {
       "/api": {
