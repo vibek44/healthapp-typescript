92c1743e8a814bc0d074934d8c84efc6e9749f92
vibek44 <kshetrivibek@gmail.com>
2026-09-19 20:53:56 +0300

    rearranging the order of middleware consumed by app and placing express.static  middleware after routes router and also declaring catch all route using splash to catch unhandled route and serving with index.html file as fallback option

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -9,13 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = 3001;
-
+app.use("/api/diagnoses", diagnosesRouter);
+app.use("/api/patients", patientsRouter);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../dist")));
-app.use("/api/diagnoses", diagnosesRouter);
-app.use("/api/patients", patientsRouter);
 
+app.get("/*splat", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 app.use(unknownEndPoint);
 app.use(errorHandler);
 app.listen(PORT, () => {
