03b6af1c0fc9921893d66dc4049972f164c5c4e4
vibek44 <kshetrivibek@gmail.com>
2026-09-23 18:26:54 +0300

    removing code to serve static file and its import like path module as deployment for the frontend and backend is done separately called decoupled deployment

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import cors from "cors";
 import diagnosesRouter from "./routes/diagnoses.ts";
 import patientsRouter from "./routes/patients.ts";
@@ -11,13 +9,7 @@ app.use(express.json());
 const PORT = 3001;
 app.use("/api/diagnoses", diagnosesRouter);
 app.use("/api/patients", patientsRouter);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("/*splat", (_req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
-});
 app.use(unknownEndPoint);
 app.use(errorHandler);
 app.listen(PORT, () => {
